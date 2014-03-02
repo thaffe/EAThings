cd46840ff786595fd40fa003e5a2e6e2b8d3dab5
= <=>
2014-03-02 16:59:14 +0100

    ?

--- FlatLand.py
+++ FlatLand.py
@@ -2,63 +2,56 @@ from random import *
 from numpy import array
 
 
-class Direction(array):
-
+class Direction:
     def __init__(self):
-        self.append(randint(-1, 1))
-        self.append(0 if self[0] else randint(-1, 1))
+        d = randint(-1, 1)
+        self.val = array([d, 0 if d else randint(-1, 1)])
 
     def turn_left(self):
-        if self[0]:
-            self[1] = self[0]
-            self[0] = 0
+        if self.val[0]:
+            self.val[1] = self[0]
+            self.val[0] = 0
         else:
-            self[0] = -self[1]
-            self[1] = 0
+            self.val[0] = -self.val[1]
+            self.val[1] = 0
 
     def turn_right(self):
-        if self[0]:
-            self[1] = -self[0]
-            self[0] = 0
+        if self.val[0]:
+            self.val[1] = -self.val[0]
+            self.val[0] = 0
         else:
-            self[0] = self[1]
-            self[1] = 0
+            self.val[0] = self.val[1]
+            self.val[1] = 0
 
 
 class FlatLand:
-
     def __init__(self, N=8, M=8, f=0.5, p=0.5):
         self.map = array([
-            ['f' if random() < f else 'p' if random() < p else None for _ in xrange(M)] for _ in xrange(N)
+            ['f' if random() < f else 'p' if random() < p else 0 for _ in xrange(M)] for _ in xrange(N)
         ])
-
-        legitimate_start_position = False
-        while not legitimate_start_position:
-            self.agent_pos = array([randint(0, N - 1), randint(0, M - 1)])
-            if not self.map[self.agent_pos]:
-                legitimate_start_position = True
-
+        while True:
+            self.agent_pos = (randint(0, N - 1), randint(0, M - 1))
+            if self.map[self.agent_pos] == '0': break;
         self.agent_direction = Direction()
-
         self.food_gathered = 0
         self.poisoned = 0
 
     def move(self, move):
         if move == 'f':
-            self.agent_pos += self.agent_direction
+            self.agent_pos += self.agent_direction.val
         elif move == 'l':
             self.agent_direction.turn_left()
-            self.agent_pos += self.agent_direction
+            self.agent_pos += self.agent_direction.val
         elif move == 'r':
             self.agent_direction.turn_right()
-            self.agent_pos += self.agent_direction
+            self.agent_pos += self.agent_direction.val
         elif move == 'n':
             pass
         else:
             raise AttributeError
 
         if not self.legitimate_position:
-            self.agent_pos -= self.agent_direction
+            self.agent_pos -= self.agent_direction.val
             return False
         elif self.map[self.agent_pos] == 'f':
             self.map[self.agent_pos] = None
@@ -72,6 +65,7 @@ class FlatLand:
         direction = self.agent_direction
 
         front_smell = self.map[self.agent_pos + direction]
+        front_smell = self.map[self.agent_pos + direction]
         direction.turn_left()
         left_smell = self.map[self.agent_pos + direction]
         right_smell = self.map[self.agent_pos - direction]
@@ -81,4 +75,7 @@ class FlatLand:
 
     @property
     def legitimate_position(self):
-        return  0 < self.agent_pos[0] < len(self.map) and 0 < self.agent_pos[1] < len(self.map[self.agent_pos[0]])
+        return 0 < self.agent_pos[0] < len(self.map) and 0 < self.agent_pos[1] < len(self.map[self.agent_pos[0]])
+
+
+FlatLand()
