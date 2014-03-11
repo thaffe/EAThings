1ba2859a8fc7be8929edf6adf0f56ad17b8ef27d
= <=>
2014-03-11 16:15:11 +0100

    Ting

--- FlatLand.py
+++ FlatLand.py
@@ -5,11 +5,11 @@ from numpy import array
 class Direction:
     def __init__(self):
         d = randint(-1, 1)
-        self.val = array([d, 0 if d else randint(-1, 1)])
+        self.val = array([d, 0 if d else 1 if random() < 0.5 else -1])
 
     def turn_left(self):
         if self.val[0]:
-            self.val[1] = self[0]
+            self.val[1] = self.val[0]
             self.val[0] = 0
         else:
             self.val[0] = -self.val[1]
@@ -25,6 +25,8 @@ class Direction:
 
 
 class FlatLand:
+    N = 8
+    M = 8
     def __init__(self, N=8, M=8, f=0.5, p=0.5):
         self.map = array([
             ['f' if random() < f else 'p' if random() < p else 0 for _ in xrange(M)] for _ in xrange(N)
@@ -64,14 +66,16 @@ class FlatLand:
 
     def smell(self):
         direction = self.agent_direction
-
-        front_smell = self.map[self.agent_pos + direction.val]
+        print(self.agent_pos, direction.val,tuple(self.agent_pos + direction.val))
+        front_smell = self.get_cell(self.agent_pos + direction.val)
         direction.turn_left()
-        left_smell = self.map[self.agent_pos + direction.val]
-        right_smell = self.map[self.agent_pos - direction.val]
+        left_smell = self.get_cell(self.agent_pos + direction.val)
+        right_smell = self.get_cell(self.agent_pos - direction.val)
 
         return [front_smell, left_smell, right_smell]
 
+    def get_cell(self, pos):
+        return 0 if pos[0] > self.M or pos[0] < 0 or pos[1] > self.N or pos[1] < 0 else self.map[tuple(pos)]
 
     @property
     def legitimate_position(self):
--- WebGui.py
+++ WebGui.py
@@ -17,9 +17,12 @@ class index:
 class flatland_web:
     def GET(self):
         i = web.input()
-        maps = [FlatLand.FlatLand().map.tolist() for _ in xrange(5)]
+        flatlands = [FlatLand.FlatLand() for _ in xrange(5)]
+        maps = [x.map.tolist() for x in flatlands]
+        print(flatlands[0].smell())
+
         b = "test"
-        return render.flatland(maps,b)
+        return render.flatland(maps, [flatlands[0].agent_pos[0], flatlands[0].agent_pos[1]])
 
 
 class beeragent_web:
